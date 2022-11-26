3babb253e04096c7d7ac7c1a9a2bcc361c982986
LucaSorann0 <118919272+LucaSorann0@users.noreply.github.com>
2022-11-26 11:46:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci (int n){
+int a = 0, b = 1, c = 0;
+cout << a + b << endl; 
+while ((a+b)<=n){
+c = a + b;
+cout << c << endl;
+a = b;
+b = c; 
+}
+return n;
+}
+
 int main() {
+   int n=0;
+cin >> n;
+fibonacci(n);
    return 0;
 }
